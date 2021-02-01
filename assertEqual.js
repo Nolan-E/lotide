21f5c2fc04f3436ac1cd63af8b1f3a36285a9cfd
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-01 16:35:38 -0700

    Added assertEqual.js - Implement assertEqual assignment for the beginning of lotide project

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATIO
+const assertEqual = function(actual, expected) {
+  let output = "";
+
+  if (actual === expected) {
+    output = "✅ Assertion Passed: " + actual + " === " + expected;
+  } else if (actual !== expected) {
+    output = "⛔️ Assertion Failed: " + actual + " !== " + expected;
+  } return output;
+};
+
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual("LHL", "LHL"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 10));
