2ccc12cdd5920810e2b2068d69a2753eaec3aafb
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-01 17:00:30 -0700

    Refactored assertEqual.js to use string literals & interpolation instead of concatenation.

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected) {
   let output = "";
 
   if (actual === expected) {
-    output = "✅ Assertion Passed: " + actual + " === " + expected;
+    output = `✅ Assertion Passed: ${actual} === ${expected}`;
   } else if (actual !== expected) {
-    output = "⛔️ Assertion Failed: " + actual + " !== " + expected;
+    output = `⛔️ Assertion Failed: ${actual} !== ${expected}`;
   } return output;
 };
 
