15f59a2b2c99ef8502ee8a9839f218b2e35bc3ca
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-02 18:24:16 -0700

    Added assertArraysEqual.js - Implement assertArraysEqual assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(Arr1, Arr2) {
+  
+  if (Arr1.length === Arr2.length){
+    for (let i = 0; i < Arr1.length; i++) {
+      if (Arr1[i] !== Arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let output = "";
+
+  if (eqArrays(actual, expected)) {
+    output = `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else if (!eqArrays(actual, expected)) {
+    output = `⛔️ Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(output);
+};
