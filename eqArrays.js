273af84216080a68ed39400fa52ade7de4771ff1
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-02 18:06:05 -0700

    Fixed some linting

--- eqArrays.js
+++ eqArrays.js
@@ -11,7 +11,7 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(Arr1, Arr2) {
 
-  if (Arr1.length === Arr2.length){
+  if (Arr1.length === Arr2.length) {
     for (let i = 0; i < Arr1.length; i++) {
       if (Arr1[i] !== Arr2[i]) {
         return false;
