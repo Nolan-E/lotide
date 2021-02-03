207d6f684ffd8da1392502dfd05e8abce3e54cc7
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-02 18:40:00 -0700

    Updated linting of assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const eqArrays = function(Arr1, Arr2) {
   
-  if (Arr1.length === Arr2.length){
+  if (Arr1.length === Arr2.length) {
     for (let i = 0; i < Arr1.length; i++) {
       if (Arr1[i] !== Arr2[i]) {
         return false;
