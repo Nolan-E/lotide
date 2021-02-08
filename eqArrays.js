0c414baca00e6d229620f0de27f4b1c28d348077
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-08 14:25:55 -0700

    Refactored eqArrays.js & moved test code into associated file in test directory.

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let output = "";
-
-  if (actual === expected) {
-    output = `✅ Assertion Passed: ${actual} === ${expected}`;
-  } else if (actual !== expected) {
-    output = `⛔️ Assertion Failed: ${actual} !== ${expected}`;
-  } return output;
-};
-
 const eqArrays = function(Arr1, Arr2) {
 
   if (Arr1.length === Arr2.length) {
@@ -23,8 +13,5 @@ const eqArrays = function(Arr1, Arr2) {
   return true;
 };
 
-// TEST CODE
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+// EXPORT MODULES
+module.exports = eqArrays;
