7b74734bb92202769af603915c4784d5da615891
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-08 19:01:00 -0700

    Refactored letterPositions.js to import/export modules. Added associated file in test directory. Updated index.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,28 +1,3 @@
-// HELPER FUNCTIONS
-const eqArrays = function(Arr1, Arr2) {
-  
-  if (Arr1.length === Arr2.length) {
-    for (let i = 0; i < Arr1.length; i++) {
-      if (Arr1[i] !== Arr2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let output = "";
-
-  if (eqArrays(actual, expected)) {
-    output = `✅ Assertion Passed: ${actual} === ${expected}`;
-  } else if (!eqArrays(actual, expected)) {
-    output = `⛔️ Assertion Failed: ${actual} !== ${expected}`;
-  }
-  console.log(output);
-};
 // FUNCTION IMPLEMENTATION
 const letterPositions = function(str) {
   const output = {};
@@ -39,26 +14,5 @@ const letterPositions = function(str) {
   return output;
 };
 
-// TEST 1 CODE
-const test1 = letterPositions("hello");
-console.log(test1);
-
-assertArraysEqual(test1.h, [0]);
-assertArraysEqual(test1.e, [1]);
-assertArraysEqual(test1.l, [2, 3]);
-assertArraysEqual(test1.o, [4]);
-
-//TEST 2 CODE
-const test2 = letterPositions("lighthouse in the house");
-console.log(test2);
-
-assertArraysEqual(test2.l, [0]);
-assertArraysEqual(test2.i, [1, 11]);
-assertArraysEqual(test2.g, [2]);
-assertArraysEqual(test2.h, [3, 5, 15, 18]);
-assertArraysEqual(test2.t, [4, 14]);
-assertArraysEqual(test2.o, [6, 19]);
-assertArraysEqual(test2.u, [7, 20]);
-assertArraysEqual(test2.s, [8, 21]);
-assertArraysEqual(test2.e, [9, 16, 22]);
-assertArraysEqual(test2.n, [12]);
+// EXPORT MODULES
+module.exports = letterPositions;
---
+++ test/letterPositions.test.js
@@ -0,0 +1,10 @@
+// IMPORT MODULES
+const assert = require('chai').assert;
+const letterPositions = require('../letterPositions');
+
+// TEST CODE
+describe("#letterPositions", () => {
+  it("returns {h: [0], e: [1], l: [2, 3] o: [4]} for 'hello'", () => {
+    assert.deepEqual(letterPositions('hello'), {h: [0], e: [1], l: [2, 3], o: [4]});
+  });
+});
